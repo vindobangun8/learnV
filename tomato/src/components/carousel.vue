25835fe03ce98f81c263a7f76000a802e6cac2ab
vindobangun8 <vindobangun8@gmail.com>
2021-01-14 21:47:07 +0700

    second

---
+++ tomato/src/components/carousel.vue
@@ -0,0 +1,40 @@
+<template>
+ 
+
+      <div class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="@/assets/food1.jpg" class="d-block w-100" alt="...">
+      
+    </div>
+    <div class="centered">
+        <h1 style="text-align:center;">Tomato</h1>
+        <h2>Finding your Favourite Restaurant</h2>
+        <div>
+        <input placeholder="Cari restauran" style="width:100%;">
+        </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Carousel"
+}
+</script>
+
+<style>
+.centered {
+  position: absolute;
+  top: 50%;
+  color:white;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.carousel-item {
+  height: 50vh;
+  min-height: 350px;
+}
+</style>
---
+++ tomato/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Carousel msg="test"/>
+    <div>coba</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Carousel from '@/components/carousel.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Carousel
+  }
+}
+</script>
